4254dcfb0788e43a4c9903b7eb1fbeb156b43cf5
Nicolas <53067913+Gumio94@users.noreply.github.com>
2019-09-03 18:16:08 -0300

    ejemplo php POO

---
+++ app/vistas/inc/login.php
@@ -0,0 +1,17 @@
+<form>
+    <div class="form-group">
+        <label for="usuario">Usuario</label>
+        <input type="text" class="form-control" id="usuario" placeholder="Ingrese usuario">
+    </div>
+    <div class="form-group">
+        <label for="contrasenia">Contraseña</label>
+        <input type="password" class="form-control" id="contrasenia" placeholder="Ingrese contraseña">
+    </div>
+    <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="recordarme">
+        <label class="form-check-label" for="recordarme">Recordarme</label>
+    </div>
+    <small id="emailHelp" class="form-text"></small>
+    <button type="submit" class="btn w-100 btn-primary">Ingresar</button>
+</form>
+
--- app/vistas/paginas/inicio.php
+++ app/vistas/paginas/inicio.php
@@ -13,11 +13,22 @@
 </nav>
 
 <div class="container-fluid">
-    <div class="row p-3 justify-content-between">
+    <div class="row justify-content-between">
         <main class="col-md-7 col-lg-8 p-3 bg-primary">
-            asdsad
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, in, sit, necessitatibus, voluptas officiis fugit iste autem expedita reprehenderit eligendi quos dicta nisi enim itaque voluptate quasi ducimus maiores fuga.</div>
+            <div>Nam, dolore, delectus perferendis tempore velit accusamus repudiandae mollitia obcaecati voluptas tenetur rerum voluptates eveniet omnis sequi enim vitae nulla. Veniam, reprehenderit dolore asperiores sed sapiente quisquam voluptate dolorem tempore?</div>
+            <div>Distinctio dolorem est pariatur velit doloremque reprehenderit. Possimus odit error nulla voluptatibus repudiandae quidem voluptatum! Rerum, dolores quidem iure mollitia sequi beatae accusantium. Quasi ex magnam neque quos saepe praesentium.</div>
+            <div>Amet, dolorem, tempora, incidunt molestias accusamus dignissimos voluptates delectus ducimus quod nulla minus autem voluptatem quasi architecto rerum ipsam cum deserunt ipsa quibusdam magni. Quibusdam, quos ullam magnam harum recusandae.</div>
+            <div>Mollitia totam tempore dolorem velit ullam doloribus sequi alias debitis voluptatem repudiandae? Voluptatum, voluptatibus, aspernatur repellat earum magni ex delectus dolorem quo explicabo eum corporis sit eos quasi saepe officia!</div>
+            <div>Eius, ea, quam, sit, in voluptates mollitia molestias suscipit culpa iure pariatur rerum laudantium quis quaerat odit excepturi hic et rem tenetur aut a error quae ad quod nostrum dicta.</div>
+            <div>Repellat, consequatur, corporis delectus doloribus voluptatibus vel unde excepturi libero cupiditate amet ratione totam itaque facilis reiciendis fugiat id expedita veritatis hic alias sint quisquam consequuntur iste ducimus at voluptates.</div>
+            <div>Consectetur, quibusdam, reiciendis, voluptatem iure velit quas itaque neque natus sint in numquam est deleniti officia dignissimos sed adipisci unde laudantium cupiditate id minus aut nemo animi recusandae dolorum veritatis!</div>
+            <div>A, similique, alias, debitis, eum saepe ad doloribus totam optio nostrum non quibusdam assumenda provident repudiandae eligendi modi voluptas explicabo amet! Sunt, sed doloribus nihil repellendus dicta quo eligendi a!</div>
+            <div>Obcaecati, delectus, suscipit aliquam voluptatibus tenetur praesentium nisi ratione quo dolore sunt quasi doloremque optio reiciendis illo nesciunt possimus quam corporis dicta itaque vero? Ratione aut veniam atque explicabo sequi.</div>
         </main>
-        <div class="col-md-4 col-lg-3 p-3 bg-secondary">sdasd</div>
+        <div class="col-md-4 col-lg-3 p-3 bg-secondary d-none d-md-block">
+            <?php require RUTA_APP.'/vistas/inc/login.php';?>
+        </div>
     </div>
 </div>
 <?php require RUTA_APP.'/vistas/inc/footer.php';?>
